12b74bd64645aec0d4979332df141d7bffb4ca6d
cosmo <applegm@me.com>
2023-11-16 16:20:16 +0800

    bugfixs

--- handlers/send_msg.go
+++ handlers/send_msg.go
@@ -248,6 +248,10 @@ func handleSendMsg(client callapi.Client, api openapi.OpenAPI, apiv2 openapi.Ope
 		echo.AddMapping(idInt64, echo.GetMapping(idInt64)-1)
 		handleSendMsg(client, api, apiv2, messageCopy)
 	}
+	//重置递归类型
+	if echo.GetMapping(idInt64) == 0 {
+		echo.AddMsgType(config.GetAppIDStr(), idInt64, "")
+	}
 }
 
 // 通过user_id获取messageID
--- handlers/send_private_msg.go
+++ handlers/send_private_msg.go
@@ -161,6 +161,10 @@ func handleSendPrivateMsg(client callapi.Client, api openapi.OpenAPI, apiv2 open
 		echo.AddMapping(idInt64, echo.GetMapping(idInt64)-1)
 		handleSendGroupMsg(client, api, apiv2, messageCopy)
 	}
+	//重置递归类型
+	if echo.GetMapping(idInt64) == 0 {
+		echo.AddMsgType(config.GetAppIDStr(), idInt64, "")
+	}
 }
 
 // 这里是只有群私聊会用到
