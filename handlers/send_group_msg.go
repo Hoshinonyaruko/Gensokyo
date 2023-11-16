4015753dbe9ee39fa7ed62e8a581a809a9ae306c
SanaeFox <36219542+Hoshinonyaruko@users.noreply.github.com>
2023-11-16 16:21:07 +0800

    Beta33 (#105)
    * bugfixs
    
    * bugfixs

--- handlers/send_group_msg.go
+++ handlers/send_group_msg.go
@@ -184,6 +184,10 @@ func handleSendGroupMsg(client callapi.Client, api openapi.OpenAPI, apiv2 openap
 		echo.AddMapping(idInt64, echo.GetMapping(idInt64)-1)
 		handleSendGroupMsg(client, api, apiv2, messageCopy)
 	}
+	//重置递归类型
+	if echo.GetMapping(idInt64) == 0 {
+		echo.AddMsgType(config.GetAppIDStr(), idInt64, "")
+	}
 
 }
 
