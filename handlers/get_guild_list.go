601c11a8fa6cedae880ed3ad6b2e6d568a3502b6
cosmo <applegm@me.com>
2023-11-19 14:14:53 +0800

    api add

--- handlers/get_guild_list.go
+++ handlers/get_guild_list.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"context"
+
 	"github.com/hoshinonyaruko/gensokyo/callapi"
 	"github.com/hoshinonyaruko/gensokyo/mylog"
+	"github.com/tencent-connect/botgo/dto"
 	"github.com/tencent-connect/botgo/openapi"
 )
 
@@ -25,32 +28,42 @@ func init() {
 }
 
 func getGuildList(client callapi.Client, api openapi.OpenAPI, apiv2 openapi.OpenAPI, message callapi.ActionMessage) {
-
 	var response GuildListResponse
 
-	// Assuming 'a' is some value you want to loop till.
-	a := 1 // Replace with appropriate value
+	// 创建一个 GuildPager 实例，设置 limit 为 10
+	pager := dto.GuildPager{Limit: "50", After: "0"} // 默认从0开始，取50个
 
-	for i := 1; i <= a; i++ {
+	// 调用 API 获取群组列表
+	guilds, err := api.MeGuilds(context.Background(), &pager)
+	if err != nil {
+		mylog.Printf("Error fetching guilds: %v", err)
+		return
+	}
+
+	// 将获取的群组数据添加到 response 中
+	for _, guild := range guilds {
 		guildData := GuildData{
-			GuildID:        "0",
-			GuildName:      "868858989",
-			GuildDisplayID: "868858989",
+			GuildID:        guild.ID,
+			GuildName:      guild.Name,
+			GuildDisplayID: guild.ID, // 或其他合适的字段
+			// ... 其他需要的字段
 		}
 		response.Data = append(response.Data, guildData)
 	}
 
+	// 设置 response 的其他属性
 	response.Message = ""
 	response.RetCode = 0
 	response.Status = "ok"
 	response.Echo = message.Echo
 
-	// Convert the members slice to a map
+	// Convert the response to a map (if needed)
 	outputMap := structToMap(response)
 
 	mylog.Printf("getGuildList(频道): %+v\n", outputMap)
 
-	err := client.SendMessage(outputMap)
+	// 发送消息
+	err = client.SendMessage(outputMap)
 	if err != nil {
 		mylog.Printf("Error sending message via client: %v", err)
 	}
