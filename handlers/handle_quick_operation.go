15abb2c259e979ad42b075911068ab87f5da7547
cosmo <applegm@me.com>
2023-11-30 14:44:53 +0800

    beta66

--- handlers/handle_quick_operation.go
+++ handlers/handle_quick_operation.go
@@ -10,14 +10,41 @@ func init() {
 }
 
 func handle_quick_operation(client callapi.Client, api openapi.OpenAPI, apiv2 openapi.OpenAPI, message callapi.ActionMessage) {
-	// 根据上下文和操作类型处理请求
-	switch message.PostType {
-	case "message":
-		switch message.MessageType {
-		case "group":
-			handleSendGroupMsg(client, api, apiv2, message)
-		case "private":
-			handleSendPrivateMsg(client, api, apiv2, message)
+	// 使用 CreateSendGroupMsgAction 函数来确定如何处理消息
+	newMsg := CreateSendGroupMsgAction(message)
+
+	// 根据返回的 ActionMessage 类型调用相应的处理函数
+	if newMsg != nil {
+		switch newMsg.Action {
+		case "send_group_msg":
+			handleSendGroupMsg(client, api, apiv2, *newMsg)
+		case "send_private_msg":
+			handleSendPrivateMsg(client, api, apiv2, *newMsg)
 		}
+	}
+}
+
+func CreateSendGroupMsgAction(originalMsg callapi.ActionMessage) *callapi.ActionMessage {
+	switch originalMsg.Params.Context.MessageType {
+	case "group":
+		return &callapi.ActionMessage{
+			Action: "send_group_msg",
+			Params: callapi.ParamsContent{
+				GroupID: originalMsg.Params.Context.GroupID,
+				Message: originalMsg.Params.Operation.Reply,
+			},
+		}
+
+	case "private":
+		return &callapi.ActionMessage{
+			Action: "send_private_msg",
+			Params: callapi.ParamsContent{
+				UserID:  originalMsg.Params.Context.UserID,
+				Message: originalMsg.Params.Operation.Reply,
+			},
+		}
+
+	default:
+		return nil // 或处理其他类型消息的逻辑
 	}
 }
