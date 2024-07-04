b790b29ec0acb0f1af93ce9f2272356912ee97c7
cosmo <applegm@me.com>
2024-07-04 16:51:02 +0800

    beta450

--- handlers/avatar.go
+++ handlers/avatar.go
@@ -27,6 +27,7 @@ func ProcessCQAvatar(groupID string, text string) string {
 				_, originalUserID, err = idmap.RetrieveRowByIDv2Pro("690426430", qqStr)
 				if err != nil {
 					mylog.Printf("Error reading private originalUserID: %v", err)
+					return ""
 				}
 			}
 		} else {
@@ -47,6 +48,70 @@ func ProcessCQAvatar(groupID string, text string) string {
 	return text
 }
 
+func ProcessCQAvatarNoGroupID(text string) string {
+	// 断言并获取 groupID 和 qq 号
+	qqRegex := regexp.MustCompile(`\[CQ:avatar,qq=(\d+)\]`)
+	qqMatches := qqRegex.FindAllStringSubmatch(text, -1)
+
+	for _, match := range qqMatches {
+		qqStr := match[1] // 提取 qq 号
+
+		var originalUserID string
+		var err error
+		if config.GetIdmapPro() {
+			_, originalUserID, err = idmap.RetrieveRowByIDv2Pro("690426430", qqStr)
+			if err != nil {
+				mylog.Printf("Error reading private originalUserID: %v", err)
+			}
+		} else {
+			originalUserID, err = idmap.RetrieveRowByIDv2(qqStr)
+			if err != nil {
+				mylog.Printf("Error retrieving original UserID: %v", err)
+			}
+		}
+
+		// 生成头像URL
+		avatarURL, _ := GenerateAvatarURLV2(originalUserID)
+
+		// 替换文本中的 [CQ:avatar,qq=12345678] 为 [CQ:image,file=avatarurl]
+		replacement := fmt.Sprintf("[CQ:image,file=%s]", avatarURL)
+		text = qqRegex.ReplaceAllString(text, replacement)
+	}
+
+	return text
+}
+
+func GetAvatarCQCodeNoGroupID(qqNumber string) (string, error) {
+	var originalUserID string
+	var err error
+
+	if config.GetIdmapPro() {
+		// 如果配置为使用Pro版本，则调用RetrieveRowByIDv2Pro
+		_, originalUserID, err = idmap.RetrieveRowByIDv2Pro("690426430", qqNumber)
+		if err != nil {
+			mylog.Printf("Error reading private originalUserID: %v", err)
+			return "", err
+		}
+	} else {
+		// 否则调用RetrieveRowByIDv2
+		originalUserID, err = idmap.RetrieveRowByIDv2(qqNumber)
+		if err != nil {
+			mylog.Printf("Error retrieving original UserID: %v", err)
+			return "", err
+		}
+	}
+
+	// 生成头像URL
+	avatarURL, err := GenerateAvatarURLV2(originalUserID)
+	if err != nil {
+		mylog.Printf("Error generating avatar URL: %v", err)
+		return "", err
+	}
+
+	// 返回格式化后的字符串
+	return fmt.Sprintf("[CQ:image,file=%s]", avatarURL), nil
+}
+
 func GetAvatarCQCode(groupID, qqNumber string) (string, error) {
 	var originalUserID string
 	var err error
@@ -56,7 +121,11 @@ func GetAvatarCQCode(groupID, qqNumber string) (string, error) {
 		_, originalUserID, err = idmap.RetrieveRowByIDv2Pro(groupID, qqNumber)
 		if err != nil {
 			mylog.Printf("Error retrieving original GroupID: %v", err)
-			return "", err
+			_, originalUserID, err = idmap.RetrieveRowByIDv2Pro("690426430", qqNumber)
+			if err != nil {
+				mylog.Printf("Error reading private originalUserID: %v", err)
+				return "", err
+			}
 		}
 	} else {
 		// 否则调用RetrieveRowByIDv2
