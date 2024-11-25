5b57e0d04b04a2b657b6f529b02e4d8aeba9065b
cosmo <applegm@me.com>
2024-11-25 16:42:03 +0800

    beta510

--- template/config_template.go
+++ template/config_template.go
@@ -86,7 +86,6 @@ settings:
   crt : ""                           #证书路径 从你的域名服务商或云服务商申请签发SSL证书(qq要求SSL) 
   key : ""                           #密钥路径 Apache（crt文件、key文件）示例: "C:\\123.key" \需要双写成\\
   webhook_path : "webhook"           #webhook监听的地址,默认\webhook
-  webhook_prefix_ip : []             #默认为空,通过webhook进行签名验证来源,设置时,只允许ip前缀的请求,不验证签名. 2024年11月22日最近的webhookip都是 183.47.105. 开始的.
   force_ssl : false                  #默认当port设置为443时启用ssl,true可以在其他port设置下强制启用ssl.
   http_port_after_ssl : "444"       # 指定启动SSL之后的备用HTTP服务器的端口号，默认为444
   
